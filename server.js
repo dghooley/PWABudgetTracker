d1fab6ddff0869f44c9ca8e58711619087cb73c2
Derick Hooley <dghooley@gmail.com>
2021-12-19 21:18:49 -0600

    deploying

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan");
+const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
